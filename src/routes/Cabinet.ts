f258c87165ab902975d5c97b2abe83bd34335da9
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-04-19 01:30:43 +0100

    Improve use of RxJS

--- src/routes/Cabinet.ts
+++ src/routes/Cabinet.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
-import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { BAD_REQUEST, OK } from 'http-status-codes';
 import { from } from 'rxjs';
+import { first, map } from 'rxjs/operators';
 
 import db from './../db/firebase-db';
 
@@ -9,10 +10,20 @@ const router = Router();
 
 
 router.get('/check', async (req: Request, res: Response) => {
-    from(db.collection('Trophies').get()).subscribe(
-        (snapshot: any) => { console.log('SNAPSHOT!') },
-        (error:    any) => { console.log('ERROR!') },
-        ()              => { res.json({'message': 'COMPLETED!'})}
+
+    from(db.collection('Trophies').get())
+    .pipe(
+        first(),
+        map((snapshot: any) => {
+            return snapshot.docs.reduce((acc: any, doc: any) => {
+                acc.push(doc.data());
+                return acc
+            }, new Array())
+        })
+    )
+    .subscribe(
+        (data:  any) => { res.status(OK).json({'data': data})},
+        (error: any) => { res.status(BAD_REQUEST).json({'error': error.message })}
     )
 });
 
